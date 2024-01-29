02ac379b16fc66bbf76267e86b721355084e90e2
shoken <shoken4797@gmail.com>
2024-01-29 11:57:33 +0900

    スマホのアドレスバーを考慮した高さ指定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -107,7 +107,7 @@ const SWrap = styled.div`
     animation-duration: 4s;
     animation-iteration-count: infinite;
     width: 100%;
-    height: 100vh;
+    height: 100svh;
     background-image: url("../../background.jpg");
     background-position: center;
     background-repeat: no-repeat;
